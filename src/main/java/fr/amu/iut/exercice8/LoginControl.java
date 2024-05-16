eb7bc9bcb4ab8b45dbb7ae29f903224181013357
Enzo <enzo.bouchami@etu.univ-amu.fr>
2024-05-16 17:31:59 +0200

    exo 8

--- src/main/java/fr/amu/iut/exercice8/LoginControl.java
+++ src/main/java/fr/amu/iut/exercice8/LoginControl.java
@@ -1,12 +1,29 @@
 package fr.amu.iut.exercice8;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 
 public class LoginControl extends GridPane {
+    @FXML
+    TextField usId=new TextField();
+    @FXML
+    PasswordField pwd = new PasswordField();
+    @FXML
+    Button ok = new Button();
+    @FXML
+    Button cancel = new Button();
 
-    private void okClicked() {
+    public void okClicked() {
+        System.out.println(usId.getText());
+
     }
 
-    private void cancelClicked() {
+    public void cancelClicked() {
+        usId.clear();
+        pwd.clear();
     }
 }
