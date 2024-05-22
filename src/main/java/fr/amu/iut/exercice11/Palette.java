4ac58260cdc3d15d35930ad85222a4fcc5d6602c
Enzo <enzo.bouchami@etu.univ-amu.fr>
2024-05-22 10:41:13 +0200

    exo11 fini

--- src/main/java/fr/amu/iut/exercice11/Palette.java
+++ src/main/java/fr/amu/iut/exercice11/Palette.java
@@ -1,4 +1,4 @@
-package fr.amu.iut.exercice1;
+package fr.amu.iut.exercice11;
 
 import javafx.application.Application;
 import javafx.geometry.Insets;
@@ -6,10 +6,12 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Paint;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
@@ -36,38 +38,67 @@ public class Palette extends Application {
 
     @Override
     public void start(Stage primaryStage) {
+        // Création d'un conteneur
         root = new BorderPane();
-
-        texteDuHaut = new Label();
-        texteDuHaut.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
-        BorderPane.setAlignment(texteDuHaut, Pos.CENTER);
-
+        //init
+        vert = new Button("vert");
+        rouge = new Button("rouge");
+        bleu = new Button("bleu");
+        Label label = new Label("Cliquez sur le bouton");
         panneau = new Pane();
-        panneau.setPrefSize(400, 200);
+        //
+        vert.addEventHandler(MouseEvent.MOUSE_CLICKED, actionEvent -> {
+            nbVert+=1;
+            label.setText( "Vert choisi "+nbVert+" fois" );
+            panneau.setStyle("-fx-background-color : green");
+            texteDuBas.setText("Le Vert est une jolie couleur !");
+            texteDuBas.setTextFill(Paint.valueOf("008000FF"));
+        });
+        rouge.addEventHandler(MouseEvent.MOUSE_CLICKED, actionEvent -> {
+            nbRouge+=1;
+            label.setText( "Rouge choisi "+nbRouge+" fois" );
+            panneau.setStyle("-fx-background-color : red");
+            texteDuBas.setText("Le Rouge est une jolie couleur !");
+            texteDuBas.setTextFill(Paint.valueOf("FF0000FF"));
+        });
+        bleu.addEventHandler(MouseEvent.MOUSE_CLICKED, actionEvent -> {
+            nbBleu+=1;
+            label.setText( "Bleu choisi "+nbBleu+" fois" );
+            panneau.setStyle("-fx-background-color : blue");
+            texteDuBas.setText("Le Bleu est une jolie couleur !");
+            texteDuBas.setTextFill(Paint.valueOf("0000FFFF"));
+        });
+        VBox basP = new VBox();
+        // partie basse
+         HBox bas = new HBox(10);
+        bas.getChildren().addAll(
+                vert,
+                rouge,
+                bleu
+        );
+        bas.setAlignment(Pos.CENTER);
+        HBox bas2 = new HBox(10);
+        texteDuBas = new Label("");
+        bas2.setAlignment(Pos.CENTER_RIGHT);
+        bas2.getChildren().add(texteDuBas);
+        basP.getChildren().addAll(
+                bas,
+                bas2
+        );
 
-        VBox bas = new VBox();
-        boutons = new HBox(10);
-        boutons.setAlignment(Pos.CENTER);
-        boutons.setPadding(new Insets(10, 5, 10, 5));
-        texteDuBas = new Label();
-        bas.setAlignment(Pos.CENTER_RIGHT);
-        bas.getChildren().addAll(boutons, texteDuBas);
-
-        vert = new Button("Vert");
-        rouge = new Button("Rouge");
-        bleu = new Button("Bleu");
-
-        /* VOTRE CODE ICI */
-
-        boutons.getChildren().addAll(vert, rouge, bleu);
-
+        BorderPane.setAlignment(label,Pos.CENTER);
+        root.setTop(label);
         root.setCenter(panneau);
-        root.setTop(texteDuHaut);
-        root.setBottom(bas);
+        bas.setPadding(new Insets(10));
+        root.setBottom(basP);
+        // Création de la scene
+        Scene scene = new Scene( root );
 
-        Scene scene = new Scene(root);
+        primaryStage.setScene( scene );
 
-        primaryStage.setScene(scene);
+        primaryStage.setTitle("Hello application");
+        primaryStage.setWidth(400);
+        primaryStage.setHeight(200);
         primaryStage.show();
     }
 }
