a92b3e85b2979f7ef142a81fcf316ce4d93f4ca2
Enzo <enzo.bouchami@etu.univ-amu.fr>
2024-05-22 09:35:45 +0200

    exo10 fini

--- src/main/java/fr/amu/iut/exercice10/Conteneurs.java
+++ src/main/java/fr/amu/iut/exercice10/Conteneurs.java
@@ -2,8 +2,15 @@ package fr.amu.iut.exercice10;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Orientation;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.net.URL;
@@ -15,6 +22,7 @@ public class Conteneurs extends Application {
 
     @Override
     public void start(Stage stage) {
+
         try {
             BorderPane root = FXMLLoader.load(getClass().getClassLoader().getResource("exercice10/ConteneursView.fxml"));
             stage.setScene(new Scene(root));
@@ -23,5 +31,6 @@ public class Conteneurs extends Application {
         } catch (Exception e) {
             e.printStackTrace();
         }
+
     }
 }
